c0dca058372a700470909d6b67ef650d15c515d6
Steven Castroverde <stevencastroverde@Stevens-iMac.local>
2017-07-26 12:31:30 -0600

    first course

---
+++ phpPractitioner/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$hoes = [
+    'Tina',
+    'Shanice',
+    'Yolanda'
+
+];
+
+$person = [
+  'age' => 25,
+    'hair' => 'black',
+    'awesome' => false
+];
+
+
+
+require'index.view.php';
