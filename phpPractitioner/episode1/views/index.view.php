364dbef3535669f0ca33199b53f68922b4280743
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2017-07-26 13:50:07 -0600

    moved reusable parts of template to partials

--- phpPractitioner/episode1/views/index.view.php
+++ phpPractitioner/episode1/views/index.view.php
@@ -1,13 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
+<?php require ('partials/head.php'); ?>
     <h1>Home</h1>
     <?php foreach ($tasks as $task) : ?>
         <li>
@@ -19,5 +10,4 @@
         </li>
     <?php endforeach; ?>
 
-</body>
-</html>
+<?php require ('partials/footer.php'); ?>
---
+++ phpPractitioner/episode1/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <small>Steven Castroverde</small>
+</footer>
+<script>
+
+</body>
+</html>
