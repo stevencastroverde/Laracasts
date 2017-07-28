80c5b13f9ac8724ca05691b19a795e099de518a7
Steven Castroverde <stevencastroverde@Stevens-Air.domain>
2017-07-27 22:11:50 -0600

    typed router and now can handle post request

--- phpPractitioner/episode1/routes.php
+++ phpPractitioner/episode1/routes.php
@@ -1,9 +1,17 @@
 <?php
 
 
-$router->define([
-    '' => 'controllers/index.php',
-    'about' => 'controllers/about.php',
-    'about/culture' => 'controllers/about-culture.php',
-    'contact' => 'controllers/contact.php'
-]);
+//$router->define([
+//    '' => 'controllers/index.php',
+//    'about' => 'controllers/about.php',
+//    'about/culture' => 'controllers/about-culture.php',
+//    'contact' => 'controllers/contact.php',
+//    'names'=> 'controllers/addName.php'
+//]);
+
+$router->get('', 'controllers/index.php');
+$router->get('about', 'controllers/about.php');
+$router->get('about/culture', 'controllers/about-culture.php');
+$router->get('contact', 'controllers/contact.php');
+
+$router->post('names', 'controllers/addName.php');
